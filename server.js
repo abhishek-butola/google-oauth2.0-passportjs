9c92d2011609e8fc51096ee4ea70f27c224be7be
Abhishek Butola <abhishekbutola08@gmail.com>
2020-04-20 14:05:02 +0530

    dock

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.json({ msg: 'hook successfully working version 4' });
+  res.json({ msg: 'hook successfully working version 5' });
 });
 
 app.use('/auth', auth);
