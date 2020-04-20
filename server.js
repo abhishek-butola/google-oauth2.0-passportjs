65310b672b2b55d8b9ea6a81006ba70cd3cd5848
Abhishek Butola <abhishekbutola08@gmail.com>
2020-04-20 14:19:08 +0530

    docker

--- server.js
+++ server.js
@@ -43,5 +43,5 @@ app.use('/auth', auth);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log('Server running at port 5000');
+  console.log(`Server running at port ${port}`);
 });
